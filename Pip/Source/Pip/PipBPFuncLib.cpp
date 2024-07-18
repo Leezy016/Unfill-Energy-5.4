473ee03628d3adce4ccad4d4dd65bbc25cf0878f
leezy016 <leezy016@foxmail.com>
2024-07-18 14:03:30 +0800

    point in circle, point polygon distance

--- Pip/Source/Pip/PipBPFuncLib.cpp
+++ Pip/Source/Pip/PipBPFuncLib.cpp
@@ -25,3 +25,45 @@ bool UPipBPFuncLib::Pip(USplineComponent* polygon, AActor* point)
 
 	return intersectionCnt % 2 == 1;;
 }
+
+float UPipBPFuncLib::BorderDist(USplineComponent* polygon, AActor* point)
+{
+	FVector p = point->GetActorLocation();
+	int32 vertexCnt = polygon->GetNumberOfSplinePoints();
+	float dist = 0.0f;
+	float distLast = 0.0f;
+
+	for (int32 i = 0; i < vertexCnt; i++)
+	{
+		FVector p1 = polygon->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World);
+		FVector p2 = polygon->GetLocationAtSplinePoint((i + 1) % vertexCnt, ESplineCoordinateSpace::World);
+
+		// calculate line p1-p2 as y=kx+b
+		float k = (p1.Y-p2.Y) / (p1.X-p2.X);
+		float b = p1.Y - p1.X * (p2.Y - p1.Y) / (p2.X-p1.X);
+
+		// vertex is segment not line
+		float distLine = fabs(k * p.X - p.Y + b) / sqrt(1 + k * k);
+		float p_p1 = FVector::Dist(p, p1);
+		float p_p2 = FVector::Dist(p, p2);
+		TArray<float> dists = {distLine,p_p1,p_p2};
+		dist=FMath::Min(dists);
+
+		if (i == 0)
+		{
+			distLast = dist;
+			continue;
+		}
+		distLast = dist <= distLast ? dist : distLast;
+	}
+	// print distLast to log
+	UE_LOG(LogTemp, Warning, TEXT("distance to border: %f"), distLast);
+	return distLast;
+}
+
+bool UPipBPFuncLib::InsideCircle(float radius, FVector2D center, AActor* point)
+{
+	FVector p = point->GetActorLocation();
+	return radius*radius > (p.X - center.X)*(p.X - center.X)+ (p.Y - center.Y) * (p.Y - center.Y);
+}
+
--- Pip/Source/Pip/PipBPFuncLib.h
+++ Pip/Source/Pip/PipBPFuncLib.h
@@ -15,9 +15,20 @@ class PIP_API UPipBPFuncLib : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
-
 public:
 
-	UFUNCTION(BlueprintCallable, meta = (ToolTip = "point in polygon\ncheck if actor inside spline", CompactNodeTitle = "Pip"))
-	static bool Pip(USplineComponent* polygon, AActor* point);	
+	UFUNCTION(BlueprintCallable, 
+		meta = (ToolTip = "point in polygon\ncheck if actor inside spline", 
+		CompactNodeTitle = "Pip"))
+	static bool Pip(USplineComponent* polygon, AActor* point);
+
+	UFUNCTION(BlueprintCallable, 
+		meta = (ToolTip = "retrun the distance to the nearest edge of a polygon\n input:\na charactor\na spline as the polygon\n", 
+		CompactNodeTitle = "BorderDist"))
+	static float BorderDist(USplineComponent* polygon, AActor* point);
+
+	UFUNCTION(BlueprintCallable,
+		meta = (ToolTip = "check if actor inside circle",
+		CompactNodeTitle = "InsideCircle"))
+	static bool InsideCircle(float radius, FVector2D center, AActor* point);
 };
